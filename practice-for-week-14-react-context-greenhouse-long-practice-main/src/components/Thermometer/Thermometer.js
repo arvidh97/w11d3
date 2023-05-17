6bc13665d7998a51549f11d2fc7cdb71dd3ee635
Arvid Hossain <arvidh97@gmail.com>
2023-05-17 16:48:47 -0400

    started bonus thermo

--- practice-for-week-14-react-context-greenhouse-long-practice-main/src/components/Thermometer/Thermometer.js
+++ practice-for-week-14-react-context-greenhouse-long-practice-main/src/components/Thermometer/Thermometer.js
@@ -1,18 +1,37 @@
 import ReactSlider from "react-slider";
 import './Thermometer.css';
 import { useClimateContext } from "../../context/ClimateContext";
+import { useEffect, useState } from "react";
 
 
 function Thermometer() {
   const { temperature, setTemperature } = useClimateContext();
+  const [desiredTemp, setDesiredTemp] = useState(temperature)
   
+  function changeTemp(temperature, desiredTemp) {
+      if (desiredTemp > temperature) {
+        while (temperature < desiredTemp) {
+          setTemperature(prevTemp => prevTemp+1)
+        }
+      }
+      else {
+        while (temperature > desiredTemp) {
+          setTemperature(prevTemp => prevTemp-1)
+        }
+      }
+  }
+
+  useEffect(() => {
+      setTimeout(changeTemp(temperature, desiredTemp), 1000)
+    }, [desiredTemp])
+
   return (
     <section>
       <h2>Thermometer</h2>
       <div className="actual-temp">Actual Temperature: {temperature}°F</div>
       <ReactSlider
         value={temperature}
-        onAfterChange={(val) => {setTemperature(val)}}
+        onAfterChange={(val) => {setDesiredTemp(val)}}
         className="thermometer-slider"
         thumbClassName="thermometer-thumb"
         trackClassName="thermometer-track"
